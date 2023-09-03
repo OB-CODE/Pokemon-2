7fe7611a10ee6c378ee19c93409fe98de39189da
Mitchell O'Brien <mitchell.s.obrien@gmail.com>
2023-09-03 21:36:09 +1000

    background applied

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,31 @@
 // import Image from 'next/image'
-import Image from 'next/image'
-
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-         Pokemon 2 - By Tina and Mitch
+    <main className="flex min-h-screen flex-col items-center w-full">
+      <div
+        className="w-full h-full absolute bg-repeat"
+        style={{ 
+          backgroundImage: "url(/tiled.png)",
+          backgroundSize: 'auto 30%',
+          // height: '800px',
+          // width: '800px' // Set the desired width and height for the background image
+        }}
+      ></div>
+
+      <div className="pt-7 z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <div className="fixed left-0 top-0 flex w-auto justify-center border-b border-blue-300 bg-gradient-to-b from-blue-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Pokemon 2 - By Tina and Mitch
         </div>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="fixed left-0 top-0 flex w-auto justify-center border-b border-red-300 bg-gradient-to-b from-red-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Hosted with{' '}
+            Hosted with{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -27,7 +36,7 @@ export default function Home() {
             />
           </a>
         </div>
-      </div> 
+      </div>
     </main>
-  )
+  );
 }
